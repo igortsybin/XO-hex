ddd01398dd246246a6db10b0589aa72b5e430614
igorts <igor.ts.job@gmail.com>
2017-03-20 18:18:59 +0300

    FieldTest with Fields  flexible size

--- tst/io/hexlet/xo/model/FieldTest.java
+++ tst/io/hexlet/xo/model/FieldTest.java
@@ -1,6 +1,5 @@
 package io.hexlet.xo.model;
 
-import io.hexlet.xo.model.exceptions.AlreadyOccupiedException;
 import io.hexlet.xo.model.exceptions.InvalidPointException;
 import org.junit.Test;
 
@@ -12,13 +11,13 @@ import static org.junit.Assert.*;
 public class FieldTest {
     @Test
     public void testGetSize() throws Exception {
-        final Field field = new Field();
+        final Field field = new Field(3);
         assertEquals(3 , field.getSize());
     }
 
     @Test
     public void testGetFigure() throws Exception {
-        final Field field = new Field ();
+        final Field field = new Field (3);
         final Point inputPoint = new Point(0, 0);
         final Figure inputFigure = Figure.O;
         field.setFigure(inputPoint, inputFigure);
@@ -28,29 +27,29 @@ public class FieldTest {
 
     @Test
     public void testGetFigureWhenFigureIsNotSet() throws Exception {
-        final Field field = new Field ();
+        final Field field = new Field (3);
         final Point inputPoint = new Point(0, 0);
         final Figure actualFigure = field.getFigure(inputPoint);
 
         assertNull (actualFigure);
     }
 
-    @Test
-    public void testGetFigureWhenAlreadyOccupied() throws Exception {
-        final Field field = new Field ();
-        final Point inputPoint = new Point(0, 0);
-        final Figure inputFigure = Figure.O;
-        field.setFigure(inputPoint, inputFigure);
-        try {
-            field.setFigure(inputPoint, inputFigure);
-            fail(); //if test achieves string 45, the string 46 will not be done
-
-        } catch (final AlreadyOccupiedException e) {}
-    }
+//    @Test
+//    public void testGetFigureWhenAlreadyOccupied() throws Exception {
+//        final Field field = new Field ();
+//        final Point inputPoint = new Point(0, 0);
+//        final Figure inputFigure = Figure.O;
+//        field.setFigure(inputPoint, inputFigure);
+//        try {
+//            field.setFigure(inputPoint, inputFigure);
+//            fail(); //if test achieves string 45, the string 46 will not be done
+//
+//        } catch (final AlreadyOccupiedException e) {}
+//    }
 
     @Test
     public void testGetFigureWhenXIsLessThenZero() throws Exception {
-        final Field field = new Field ();
+        final Field field = new Field (3);
         final Point inputPoint = new Point(-1, 0);
         try {
             final Figure actualFigure = field.getFigure(inputPoint);
@@ -61,7 +60,7 @@ public class FieldTest {
     }
     @Test
     public void testGetFigureWhenXIsMoreThenSize() throws Exception {
-        final Field field = new Field();
+        final Field field = new Field(3);
         final Point inputPoint = new Point(field.getSize() + 1, 0);
         try {
             final Figure actualFigure = field.getFigure(inputPoint);
@@ -73,7 +72,7 @@ public class FieldTest {
 
     @Test
     public void testGetFigureWhenYIsLessThenZero() throws Exception {
-        final Field field = new Field ();
+        final Field field = new Field (3);
         final Point inputPoint = new Point(0, -1);
         try {
             final Figure actualFigure = field.getFigure(inputPoint);
@@ -85,7 +84,7 @@ public class FieldTest {
 
     @Test
     public void testGetFigureWhenYIsMoreThenSize() throws Exception {
-        final Field field = new Field();
+        final Field field = new Field(3);
         final Point inputPoint = new Point(0, field.getSize() + 1);
         try {
             final Figure actualFigure = field.getFigure(inputPoint);
